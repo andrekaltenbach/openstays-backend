a8d2b3e5675543ac7eb4ef23cb176d5f022af002
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-06-10 13:11:33 +0200

    feat: add field to Post model

--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -4,21 +4,26 @@ import prisma from '../db';
 
 const router = express.Router();
 
-router.post('/reviews', (req: Request, res: Response, next: NextFunction) => {
-  const newReview = req.body;
-  prisma.review
-    .create({ data: newReview })
-    .then((review) => {
-      console.log('Review created successfully:', review);
-      res.status(201).json(review);
-    })
-    .catch((error) => {
-      console.error('Error creating review:', error);
-      next(error);
-    });
+router.post('/posts/:postId/reviews', (req: Request, res: Response, next: NextFunction) => {
+  const { postId } = req.params;
+  if (postId) {
+    const newReview = req.body;
+    prisma.review
+      .create({ data: newReview })
+      .then((review) => {
+        console.log('Review created successfully:', review);
+        res.status(201).json(review);
+      })
+      .catch((error) => {
+        console.error('Error creating review:', error);
+        next(error);
+      });
+  } else {
+    res.status(400).json({ error: 'Post ID is required' });
+  }
 });
 
-router.get('/reviews', (req: Request, res: Response, next: NextFunction) => {
+router.get('/posts/:postId/reviews', (req: Request, res: Response, next: NextFunction) => {
   prisma.review
     .findMany({ include: { post: true } })
     .then((reviews) => {
@@ -30,53 +35,62 @@ router.get('/reviews', (req: Request, res: Response, next: NextFunction) => {
     });
 });
 
-router.get('/reviews/:reviewId', (req: Request, res: Response, next: NextFunction) => {
-  const { reviewId } = req.params;
-  prisma.review
-    .findUnique({
-      where: { id: reviewId },
-      include: { post: true },
-    })
-    .then((review) => {
-      if (!review) {
-        return res.status(404).json({ error: 'Review not found' });
-      }
-      console.log('Review fetched successfully:', review);
-      res.json(review);
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
+router.get(
+  '/posts/:postId/reviews/:reviewId',
+  (req: Request, res: Response, next: NextFunction) => {
+    const { reviewId } = req.params;
+    prisma.review
+      .findUnique({
+        where: { id: reviewId },
+        include: { post: true },
+      })
+      .then((review) => {
+        if (!review) {
+          return res.status(404).json({ error: 'Review not found' });
+        }
+        console.log('Review fetched successfully:', review);
+        res.json(review);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
 
-router.put('/reviews/:reviewId', (req: Request, res: Response, next: NextFunction) => {
-  const { reviewId } = req.params;
-  const updatedReview = req.body;
-  prisma.review
-    .update({
-      where: { id: reviewId },
-      data: updatedReview,
-    })
-    .then((review) => {
-      console.log('Review updated successfully:', review);
-      res.json(review);
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
+router.put(
+  '/posts/:postId/reviews/:reviewId',
+  (req: Request, res: Response, next: NextFunction) => {
+    const { reviewId } = req.params;
+    const updatedReview = req.body;
+    prisma.review
+      .update({
+        where: { id: reviewId },
+        data: updatedReview,
+      })
+      .then((review) => {
+        console.log('Review updated successfully:', review);
+        res.json(review);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
 
-router.delete('/reviews/:reviewId', (req: Request, res: Response, next: NextFunction) => {
-  const { reviewId } = req.params;
-  prisma.review
-    .delete({ where: { id: reviewId } })
-    .then((review) => {
-      console.log('Review deleted successfully:', review);
-      res.status(204).send();
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
+router.delete(
+  '/posts/:postId/reviews/:reviewId',
+  (req: Request, res: Response, next: NextFunction) => {
+    const { reviewId } = req.params;
+    prisma.review
+      .delete({ where: { id: reviewId } })
+      .then((review) => {
+        console.log('Review deleted successfully:', review);
+        res.status(204).send();
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
 
 export default router;
